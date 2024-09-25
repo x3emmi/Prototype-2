689f0376ee2972c57d98963531f3ad8c047d722c
Tom Consumes from Nutrition Impossible <146956075+x3emmi@users.noreply.github.com>
2024-09-25 12:28:11 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        // Keep the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
