8fff9d938203fe855b3b51cd95d4404f24b991f1
Tom Consumes from Nutrition Impossible <146956075+x3emmi@users.noreply.github.com>
2024-09-27 12:40:18 -0400

    two point two
    woah.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
     // Update is called once per frame
     void Update()
     {
@@ -27,6 +28,14 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+            
         }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+               //Launch a projectile from the player
+               Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            }
+
     }
 }
