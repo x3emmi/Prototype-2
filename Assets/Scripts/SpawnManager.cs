0a19ca096dcfb43cb7e8b88c303e5bf73618ac06
Tom Consumes from Nutrition Impossible <146956075+x3emmi@users.noreply.github.com>
2024-10-02 12:34:08 -0400

    Two Point Four
    I hate typing like that.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     public GameObject[] animalPrefabs;
@@ -17,15 +19,17 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
             // Randomly generate animal index and spawn position
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
             0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], spawnPos,
             animalPrefabs[animalIndex].transform.rotation);
-        }
-        
     }
+
 }
