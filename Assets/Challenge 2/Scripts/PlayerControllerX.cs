0001666efe0292c2ba3fe2355f625d6fbc19047a
Tom Consumes from Nutrition Impossible <146956075+x3emmi@users.noreply.github.com>
2024-10-09 12:38:46 -0400

    2.challenge
    name

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float minSpawnDogTime = 1f;
+    private float lastSpawnDogTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastSpawnDogTime += Time.deltaTime;
+        // On spacebar press, send dog
+        if (lastSpawnDogTime > minSpawnDogTime)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            lastSpawnDogTime = 0;
+            Instantiate(dogPrefab, transform.position,
+            dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private int[] startInterval = new int[2] {0, 4};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight),
+        spawnPosY, 0);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
+        //Uses values in startInterval to generate random spawn times
+        float timeGap = Random.Range(startInterval[0],
+        startInterval[startInterval.Length - 1]);
+        Invoke ("SpawnRandomBall", timeGap);
+    }
+
+}
